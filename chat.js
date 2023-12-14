62e1c12c1b43c52f8074d8ecc4a907f13d4ed266
3KR4 <mouhamedmahmoud820@gmail.com>
2023-12-15 00:21:49 +0200

    nagi-1

---
+++ chat.js
@@ -0,0 +1,71 @@
+function chatId () {
+  let urlParams = new URLSearchParams(window.location.search)
+  let userChatId = urlParams.get("userid")
+  return userChatId
+}
+let userId = chatId()
+
+let user = friendss.find((x) => x.id == userId)
+
+let pageHolder = document.querySelector(".pageHolder")
+singleChatPage = () => {
+  return (pageHolder.innerHTML = `
+    <div class="topBar">
+      <div class="user">
+        <img src="${user.img}" alt="">
+        <div>
+          <h5>${user.name}</h5>
+          <h5>online</h5>
+        </div>
+      </div>
+      <div class="icons">
+        <i class="bi bi-camera-video"></i>
+        <i class="bi bi-telephone"></i>
+        <i class="bi bi-search"></i>
+      </div>
+    </div>
+    <div class="chatHolder">
+                                        
+    </div>
+    <div class="bottomBar">
+      <i class="bi bi-emoji-smile"></i>
+      <i class="bi bi-link-45deg insert"></i>
+      <textarea type="text" id="mainChatInput" placeholder="Type a message"></textarea>
+      <div class="sendHolder">
+        <i class="bi bi-mic mic"></i>
+        <i class="bi bi-send send"></i>
+      </div>
+    </div>
+  `);
+};
+singleChatPage();
+
+let mainChatInput = document.getElementById("mainChatInput")
+let sendHolder = document.querySelector(".sendHolder")
+let sendBtn = document.querySelector(".sendHolder .send")
+
+
+mainChatInput.addEventListener("keydown", () => {
+  if(mainChatInput.value.length > 0) {
+    sendHolder.classList.add("active")
+  } else {
+    sendHolder.classList.remove("active")
+  }
+})
+
+sendBtn.addEventListener("click", () => {
+  let mainChatInputvalue = mainChatInput.value
+  document.querySelector(".chatHolder") .innerHTML += `
+  <div class="message">
+    <img src="img/one piece 2.jpg" alt="">
+    <div class="holder">
+      <h6 class="userName">Ahmed Hani</h6>
+      <h6>${mainChatInputvalue}</h6>
+    </div>
+  </div>
+  `
+  sendHolder.classList.remove("active")
+  mainChatInput.value = ""
+})
+
+// document.querySelector(".chatHolder .message").classList.add("right")
---
+++ nagi.js
@@ -0,0 +1,52 @@
+let searchHolder = document.querySelector(".users .search")
+searchHolderr = () => {
+  return (searchHolder.innerHTML = `
+    <i class="bi bi-search"></i>
+    <input type="text" id="searchInput" placeholder="Search or start a new chat">
+    <i class="bi bi-x xclose"></i>
+  `);
+};
+searchHolderr();
+
+let searchInput = document.getElementById("searchInput")
+let deleteSearchX = document.querySelector(".users .xclose")
+searchInput.addEventListener("keydown", () => {
+  if(searchInput.value.length > 0) {
+    deleteSearchX.style.display = "block"
+  } else {
+    deleteSearchX.style.display = "none"
+  }
+})
+deleteSearchX.addEventListener("click", () => {
+  searchInput.value = ""
+  deleteSearchX.style.display = "none"
+})
+
+let friendsHolder = document.querySelector(".friends")
+function rednderfilterBtn() {
+  
+  for (friend of friendss) {
+    let friendssss = `
+    <div onclick="openChat(${friend.id})" class="friend">
+    <div class="holder">
+      <img src="${friend.img}" alt="">
+        <div>
+          <h5>${friend.name}</h5>
+          <p>${friend.title}</p>
+        </div>
+      </div>
+      <div class="info">
+        <h6>${friend.time} PM</h6>
+        <span>${friend.message}</span>
+      </div>
+    </div>
+    `
+    friendsHolder.innerHTML += friendssss
+  }
+  }
+rednderfilterBtn();
+
+
+function openChat (id) {
+  window.location = `chat.html?userid=${id}`
+}
